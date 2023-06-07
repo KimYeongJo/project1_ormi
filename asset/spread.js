f7569d5d1946691004c585ce848da6ea564f7b27
KimYeongJo <132258199+KimYeongJo@users.noreply.github.com>
2023-06-07 17:59:02 +0900

    토글 구현 완료

--- asset/spread.js
+++ asset/spread.js
@@ -3,7 +3,7 @@ function spreadData(aiData) {
     const $body = document.querySelector("body")
     const div = document.createElement("div")
     div.setAttribute("id", "display")
-    div.classList.add('flex', 'flex-col', 'pl-20', 'ml-20')
+    div.classList.add('flex', 'flex-col', 'px-20', 'mx-20', 'mt-20')
     $body.append(div) 
     const $display = document.getElementById("display")
     
@@ -13,28 +13,27 @@ function spreadData(aiData) {
         const id = aiData[i]
         const key = Object.keys(aiData)[j]
         const problemBox = document.createElement("div")
-        problemBox.classList.add('flex', 'flex-col', 'mt-3', 'border-black', 'border-solid', 'border-2')
+        problemBox.classList.add('flex', 'flex-col', 'mt-3', 'p-2', 'border-gray', 'border-solid', 'border-2', 'rounded-lg')
         
         const problem = document.createElement("h2")
         problem.textContent = `문제 : ${id["문제"]}`
         
         const solve = document.createElement("textarea")
         solve.innerText = ''
-        solve.classList.add('border-black', 'border-solid', 'border-2')
+        solve.classList.add('mt-3', 'border-white', 'border-solid', 'border-2', 'rounded-lg', 'text-black')
         
         const show = document.createElement("button")
-        show.setAttribute("id", "btn_toggle")
-        show.setAttribute("onclick", `$('#${key}').toggle()`)
-        show.textContent = '정답 보기'
-        show.classList.add('border-black', 'border-solid', 'border-2')
+        show.setAttribute("onclick", `answerToggle("${key}")`)
+        show.textContent = '정답 보기/숨기기'
+        show.classList.add('my-2')
         
         const answer = document.createElement("p")
-        answer.setAttribute("id", `${key}`)
+        answer.setAttribute("name", key)
         answer.setAttribute("style", "display: none")
         answer.textContent = `정답 : ${id["정답"]}`
         
         const solution = document.createElement("p")
-        solution.setAttribute("id", `${key}`)
+        solution.setAttribute("name", key)
         solution.setAttribute("style", "display: none")
         solution.textContent = `해설 : ${id["해설"]}`
         
---
+++ asset/toggle.js
@@ -0,0 +1,11 @@
+function answerToggle(name) {
+    const $key = document.getElementsByName(name)
+    $key.forEach((toggle) => {
+        if (toggle.style.display == 'none') {
+            toggle.style.display = 'block'
+        }
+        else {
+            toggle.style.display = 'none'
+        }
+    })
+}
